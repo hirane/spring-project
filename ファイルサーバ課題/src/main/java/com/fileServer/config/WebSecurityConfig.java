9b086525faf16f7ad0251fb69b22aa91cf5d9a8e
miyatamasayuki <75602778+miyatamasayuki@users.noreply.github.com>
2020-12-17 19:02:13 +0900

    m-miyata

--- ファイルサーバ課題/src/main/java/com/fileServer/config/WebSecurityConfig.java
+++ ファイルサーバ課題/src/main/java/com/fileServer/config/WebSecurityConfig.java
@@ -15,55 +15,61 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
-	@Autowired
+    @Autowired
     UserDetailsService userDetailsService;
 
-	@Override
-	protected void configure(HttpSecurity http)throws Exception{
-		http.formLogin()
-			//ログインページを指定
-			.loginPage("/login")
-			//認証処理に移るためのURL(ログインフォームのaction先と同じに設定)
-			.loginProcessingUrl("/authenticate")
-			//ログインフォームのユーザ名入力欄name
-		    .usernameParameter("userName")
-		    //ログインフォームのパスワード入力欄name
-		    .passwordParameter("password")
-		    //ログイン成功時の遷移先
-		    .defaultSuccessUrl("/fileView",true)
-		    //ログインページのアクセスは全員許可する
-		    .permitAll();
+    @Override
+    protected void configure(HttpSecurity http)throws Exception{
+        http.formLogin()
+            //ログインページを指定
+            .loginPage("/login")
+            //認証処理に移るためのURL(ログインフォームのaction先と同じに設定)
+            .loginProcessingUrl("/authenticate")
+            //ログインフォームのユーザ名入力欄name
+            .usernameParameter("userId")
+            //ログインフォームのパスワード入力欄name
+            .passwordParameter("password")
+            //ログイン成功時の遷移先
+            .defaultSuccessUrl("/fileView",true)
+            //ログインページのアクセスは全員許可する
+            .permitAll();
 
-		http.csrf().disable().authorizeRequests()
-			//各リクエストのアクセス権限　※全リクエスト全権限に許可
-			//新規登録画面遷移
-		 	.antMatchers("/regist").permitAll()
-		 	//ユーザ管理画面遷移
-		 	.antMatchers("/userList").permitAll()
-		 	//ユーザ権限変更
-		 	.antMatchers("/editAuth").permitAll()
-		 	//ユーザ削除
-		 	.antMatchers("/delete").permitAll()
-		 	//ユーザ情報更新画面遷移
-		 	.antMatchers("/updateUser").permitAll()
-		 	//ユーザ名更新
-		 	.antMatchers("/updateName").permitAll()
-		 	//ユーザパスワード更新
-		 	.antMatchers("/updatePassword").permitAll()
-		 	//ファイル一覧画面遷移
-		 	.antMatchers("/fileView").permitAll()
-		 	//すべてのリクエストは認証されたユーザのみ承認
-		 	.anyRequest().authenticated();
-	}
+        http.logout()
+            //セッションの値を削除する
+            .invalidateHttpSession(true)
+            //ログアウト時の遷移先
+            .logoutSuccessUrl("/login");
 
-		@Bean
-		PasswordEncoder passwordEncoder(){
-			return new BCryptPasswordEncoder();
-		}
+        http.csrf().disable().authorizeRequests()
+            //各リクエストのアクセス権限　※全リクエスト全権限に許可
+            //新規登録画面遷移
+            .antMatchers("/regist").permitAll()
+            //ユーザ管理画面遷移
+            .antMatchers("/userList").permitAll()
+            //ユーザ権限変更
+            .antMatchers("/editAuth").permitAll()
+            //ユーザ削除
+            .antMatchers("/delete").permitAll()
+            //ユーザ情報更新画面遷移
+            .antMatchers("/updateUser").permitAll()
+            //ユーザ名更新
+            .antMatchers("/updateName").permitAll()
+            //ユーザパスワード更新
+            .antMatchers("/updatePassword").permitAll()
+            //ファイル一覧画面遷移
+            .antMatchers("/fileView").permitAll()
+            //すべてのリクエストは認証されたユーザのみ承認
+            .anyRequest().authenticated();
+    }
 
-		@Autowired
-    	void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
-        	auth.userDetailsService(userDetailsService)
-            	.passwordEncoder(passwordEncoder());
+        @Bean
+        PasswordEncoder passwordEncoder(){
+            return new BCryptPasswordEncoder();
+        }
+
+        @Autowired
+        void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 }
